cb874219ba943d8968e4baef5910d786e61d2832
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-12-14 14:27:25 +0100

    POO Empezado

---
+++ src/Perro.java
@@ -0,0 +1,81 @@
+import java.util.Random;
+
+public class Perro {
+
+	
+
+	private String nombre;
+	private int numChip;
+	private int edad;
+	
+	public Perro() {
+		nombre = "";
+		edad = 0;
+		numChip = 0;
+	}
+
+	public Perro(String nombre, int edad) {
+		this.nombre = nombre;
+		this.edad = edad;
+		numChip = 0;
+	}
+
+	public Perro(String nombre, int numChip, int edad) {
+		super();
+		this.nombre = nombre;
+		this.numChip = numChip;
+		this.edad = edad;
+	}
+	
+	public void ponerChip() {
+		
+		if(numChip != 0) {
+			System.out.println("Este perro ya tiene chip");
+			
+		}else {
+			Random r = new Random();
+			this.numChip = r.nextInt(100) + 1;
+		}
+		}
+	
+	public boolean comprobarChip() {
+		if (numChip%2 == 0) {
+			return true;
+			
+		}else {
+			return false;
+		}
+	}
+	
+	
+	public String getNombre() {
+		return nombre.toUpperCase();
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumChip() {
+		return numChip;
+	}
+
+	public void setNumChip(int numChip) {
+		this.numChip = numChip;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return nombre + "..... numChip=" + numChip + "...... edad=" + edad  ;
+	}
+	
+	
+}
---
+++ src/Principal.java
@@ -0,0 +1,28 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Perro p = new Perro();
+		Perro p2 = new Perro("Kacper", 5);
+		Perro p3 = new Perro("Kacper", 500, 10);
+		Perro pka = new Perro("Minuto", 8, 2);
+		
+		Perro vPerros[] = new Perro[10];
+		
+		vPerros[0] = p;
+		vPerros[1] = new Perro("Juan", 500, 10);
+		vPerros[2] = new Perro("Juan", 501, 10);
+		vPerros[3] = new Perro("Juan", 502, 10);
+		
+		for (int i = 0; i < vPerros.length; i++) {
+			if(vPerros[i] != null && vPerros[i].comprobarChip()) {
+				System.out.println(vPerros[i].toString());
+			}
+			
+		}
+		
+
+	}
+
+}
